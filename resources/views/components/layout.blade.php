86bc7334cbb7e64ed7ac10d2f11f9e2e223a8778
Dagbaev Boris <sblrhr@gmail.com>
2023-11-06 01:40:00 +0800

    added first admin page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,53 @@
+@include('components.head')
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">    
+
+        @include('components.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                @include('components.topbar')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    @yield('content')
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            @include('components.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    @include('components.scroll-to-top')
+
+    @include('components.logout-modal')
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
+
+</body>
